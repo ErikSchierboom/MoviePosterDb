51cc7115d5f2c061ad7302626a069701569310df
Erik Schierboom <erik_schierboom@hotmail.com>
2014-02-10 16:34:12 +0100

    Improved display of view model properties

--- samples/MoviePosterDbDemo/Models/IndexViewModel.cs
+++ samples/MoviePosterDbDemo/Models/IndexViewModel.cs
@@ -15,18 +15,23 @@
         }
 
         [Required]
+        [Display(Name = "API key")]
         public string ApiKey { get; set; }
 
         [Required]
+        [Display(Name = "API secret")]
         public string ApiSecret { get; set; }
 
         [Min(1)]
+        [Display(Name = "IMDb movie ID")]
         public int? ImdbMovieId { get; set; }
-        
+
         [RegularExpression(@"https?://.*?imdb.com/title/tt(\d{7})/?.*", ErrorMessage = "Not a valid IMDb movie URL.")]
+        [Display(Name = "IMDb movie URL")]
         public string ImdbMovieUrl { get; set; }
 
         [Range(30, 300)]
+        [Display(Name = "Image width")]
         public int ImageWidth { get; set; }
 
         public MoviePosterDbResult MoviePosterDbResult { get; set; }
